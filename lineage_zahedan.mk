618d9ae6f15bb6034ec762b3297ef0acb66806f6
sajjad85gh <sajjad.ghaderi.1385@gmail.com>
2025-07-22 15:24:00 +0330

    zahedan: RisingOS Bring up

--- lineage_zahedan.mk
+++ lineage_zahedan.mk
@@ -11,10 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/daria/zahedan/device.mk)
 
-# Inherit some common LMODroid stuff.
-$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lmodroid_zahedan
+# Lunch banner maintainer variable
+RISING_MAINTAINER="Itis_Sajjad"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# Set RISING_MAINTAINER for version control 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="MediaTek Dimensity 7050" \
+    RisingMaintainer="Itis_Sajjad"
+
+RISING_MAINTAINER := Itis_Sajjad
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+PRODUCT_NAME := lineage_zahedan
 PRODUCT_DEVICE := zahedan
 PRODUCT_MANUFACTURER := Daria
 PRODUCT_BRAND := Daria
